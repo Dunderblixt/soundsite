557c10fa311631faee59f048f51d727fb15524da
Dunderblixt <stalhane@gmail.com>
2026-03-10 11:53:16 +0100

    Update audioApi.js

---
+++ audioApi.js
@@ -0,0 +1,32 @@
+//
+const myTopURL = 'https://api.spotify.com/v1/me/top/artists';
+const searchURL = 'https://api.spotify.com/v1/search?';
+let endpoint;
+const query = this.querySelector('input[type="text"]').value
+// Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
+const token = 'undefined';
+async function fetchWebApi(endpoint, method, body) {
+  const res = await fetch(`${myTopURL}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method,
+    body:JSON.stringify(body)
+  });
+  return await res.json();
+}
+
+async function getTopTracks(){
+  // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+  return (await fetchWebApi(
+    'v1/me/top/tracks?time_range=long_term&limit=5', 'GET'
+  )).items;
+}
+
+const topTracks = await getTopTracks();
+console.log(
+  topTracks?.map(
+    ({name, artists}) =>
+      `${name} by ${artists.map(artist => artist.name).join(', ')}`
+  )
+);
