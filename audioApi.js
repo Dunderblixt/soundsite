88e975e32e1c24e2552e45a88168cf186e5f00ea
mohammed-alpha91 <mohammad.alpha@gritacademy.se>
2026-03-11 15:59:47 +0100

    lagt till id för albumlist på knapp i html samt fixat js kod för hämta och rendera top 20 album

--- audioApi.js
+++ audioApi.js
@@ -41,3 +41,56 @@ topArtistsBtn.addEventListener("click", function(e) {
   script.src = "https://api.deezer.com/chart/0/artists?limit=20&output=jsonp&callback=showArtists";
   document.body.appendChild(script);
 });
+
+
+// Callback global för JSONP
+window.showTopAlbums = function(response) {
+  const albums = response.data;
+  console.log("Top 20 Albums:", albums);
+
+  // Hitta id från HTML
+  const list = document.getElementById("albumList");
+  if (!list) return;
+  list.innerHTML = ""; 
+
+  // Skapar albumkort
+  albums.forEach(album => {
+    const card = document.createElement("div");
+    card.classList.add("artist-card"); // återanvänder samma CSS
+
+    // Album namn
+    const name = document.createElement("h3");
+    name.textContent = album.title;
+    card.appendChild(name);
+
+    // Album bild (medium)
+    if (album.cover_medium) {
+      const img = document.createElement("img");
+      img.src = album.cover_medium;
+      img.alt = album.title;
+      card.appendChild(img);
+    }
+
+
+    // Artist namn
+    if (album.artist && album.artist.name) {
+      const artistName = document.createElement("p");
+      artistName.textContent = `Artist: ${album.artist.name}`;
+      card.appendChild(artistName);
+    }
+
+    list.appendChild(card);
+  });
+};
+
+// Eventlistener för knappen top50Btn
+const top50Btn = document.getElementById("top50Btn");
+top50Btn.addEventListener("click", function(e) {
+  e.preventDefault();
+  console.log("Top 20 Albums knapp klickad");
+
+  
+  const script = document.createElement("script");
+  script.src = "https://api.deezer.com/chart/0/albums?limit=20&output=jsonp&callback=showTopAlbums";
+  document.body.appendChild(script);
+});
